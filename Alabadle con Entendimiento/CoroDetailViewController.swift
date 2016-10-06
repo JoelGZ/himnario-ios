a53b1a444c80386ad18d5cb30f763af7f000b5a1
Joel García <joelgarcia@Joels-Mac.local>
2016-10-05 19:52:39 -0600

    Add CoroDetailViewController. Finish setup on LetraScene for coro detail

---
+++ Alabadle con Entendimiento/CoroDetailViewController.swift
@@ -0,0 +1,127 @@
+//
+//  CoroDetailViewController.swift
+//  Alabadle con Entendimiento
+//
+//  Created by Joel García on 10/5/16.
+//  Copyright © 2016 Joel García. All rights reserved.
+//
+
+import UIKit
+
+class CoroDetailViewController: UIViewController, UIAlertViewDelegate {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nombreCoroLabel: UILabel!
+    @IBOutlet weak var numeroCoroLabel: UILabel!
+    @IBOutlet weak var infoGeneralLabel: UILabel!
+    @IBOutlet weak var tonalidadTituloLabel: UILabel!
+    @IBOutlet weak var tonalidadLabel: UILabel!
+    @IBOutlet weak var tonAltTituloLabel: UILabel!
+    @IBOutlet weak var tonAltLabel: UILabel!
+    @IBOutlet weak var velocidadTituloLabel: UILabel!
+    @IBOutlet weak var velocidadLabel: UILabel!
+    @IBOutlet weak var tiempoTituloLabel: UILabel!
+    @IBOutlet weak var tiempoLabel: UILabel!
+    @IBOutlet weak var letraCoroTituloLabel: UILabel!
+    @IBOutlet weak var letraCoroLabel: UILabel!
+    @IBOutlet weak var historiaCoroTituloLabel: UILabel!
+    @IBOutlet weak var citaTituloLabel: UILabel!
+    @IBOutlet weak var citaLabel: UILabel!
+    @IBOutlet weak var autorLetraTituloLabel: UILabel!
+    @IBOutlet weak var autorLetraLabel: UILabel!
+    @IBOutlet weak var autorMusicaTituloLabel: UILabel!
+    @IBOutlet weak var autorMusicaLabel: UILabel!
+    @IBOutlet weak var historiaLabel: UILabel!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    // Coro que viene del table view
+    var coro:Coro?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup content of labels
+        self.setupViews()
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.tabBarController?.tabBar.hidden = false
+    }
+    
+    func setupViews(){
+        
+        //Localize
+        //Titulos 2
+        infoGeneralLabel.text = "Informacion General"
+        letraCoroTituloLabel.text = "Letra"
+        historiaCoroTituloLabel.text = "Historia"
+        
+        
+        //Titulos caracteristicas
+        tonalidadTituloLabel.text = "Tonalidad:"
+        tonAltTituloLabel.text = "Tonalidad Alternativa:"
+        velocidadTituloLabel.text = "Velocidad:"
+        tiempoTituloLabel.text = "Tiempo:"
+        autorLetraTituloLabel.text = "Autor Letra:"
+        autorMusicaTituloLabel.text = "Autor Musica:"
+        
+        //Cambios dinamicos
+        nombreCoroLabel.text = coro!.nombre
+        
+        numeroCoroLabel.text = String(coro!._id)
+        tonalidadLabel.text = "\(coro!.tonalidad.getReadableText()) (\(coro!.tonalidad))"
+        if coro!.ton_alt != "" {
+            tonAltLabel.text = "\(coro!.ton_alt.getReadableText()) (\(coro!.ton_alt))"
+        } else {
+            tonAltLabel.text = ""
+        }
+        
+        velocidadLabel.text = coro!.velletra.getReadableText()
+        tiempoLabel.text = String(coro!.tiempo)
+        
+        letraCoroLabel.lineBreakMode = .ByWordWrapping
+        letraCoroLabel.numberOfLines = 0
+        letraCoroLabel.text = coro!.cuerpo
+        
+        if (coro!.cita == "") {
+            citaTituloLabel.hidden = true
+            citaLabel.hidden = true
+        } else {
+            citaTituloLabel.text = "Cita Biblica:"
+            citaLabel.text = coro!.cita
+        }
+        
+        if (coro!.autorletra == "") {
+            autorLetraLabel.text = "Desconocido"
+        } else {
+            autorLetraLabel.text = coro!.autorletra
+        }
+        
+        if (coro!.autormusica == "") {
+            autorMusicaLabel.text = "Desconocido"
+        } else {
+            autorMusicaLabel.text = coro!.autormusica
+        }
+        
+        if (coro!.historia != "") {
+            self.historiaLabel.text = coro!.historia
+        } else {
+            self.historiaLabel.hidden = true
+        }
+    }
+    
+    @IBAction func tonAltInfoAction(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "Tonalidades Alternativas", message: "Se recomienda siempre cantar los coros en su tonalidad original.", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        alert.popoverPresentationController?.sourceView = self.view
+        alert.popoverPresentationController?.sourceRect = self.view.bounds
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+}
+
