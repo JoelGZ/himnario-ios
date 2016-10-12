c7099a407891208a37136ae9a2fd63340db539e6
Joel García <joelgarcia@Joels-Mac.local>
2016-10-11 22:31:46 -0600

    Add contracts.

---
+++ Alabadle con Entendimiento/CorosContract.swift
@@ -0,0 +1,30 @@
+//
+//  CorosContract.swift
+//  Alabadle con Entendimiento
+//
+//  Created by Joel García on 10/11/16.
+//  Copyright © 2016 Joel García. All rights reserved.
+//
+
+import Foundation
+
+class CorosContract {
+    
+    let TABLE_NAME = "coros"
+    
+    let KEY_ORDEN = "orden"
+    let KEY_NOMBRE = "nombre"
+    let KEY_CUERPO = "cuerpo"
+    let KEY_TONALIDAD = "ton"
+    let KEY_TONALIDAD_ALTERNATIVA = "ton_alt"
+    let KEY_VELOCIDAD_LETRA = "vel_letra"
+    let KEY_TIEMPO = "tiempo"
+    let KEY_PARTITURA = "partitura"
+    let KEY_AUDIO = "audio"
+    let KEY_AUTOR_MUSICA = "aut_mus"
+    let KEY_AUTOR_LETRA = "aut_let"
+    let KEY_CITA = "cita"
+    let KEY_HISTORIA = "historia"
+    let KEY_SEARCHABLE_NAME = "sName"
+    
+}
